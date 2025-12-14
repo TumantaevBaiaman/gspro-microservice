543025128f5c26e785575d2c66528c9b9cb2442d
TumantaevBaiaman <tumantaev.baiaman.space@gmail.com>
2025-12-14 14:57:37 +0600

    refactor: name routers

--- gateway/app/main.py
+++ gateway/app/main.py
@@ -1,17 +1,17 @@
 from fastapi import FastAPI
 from starlette.middleware import Middleware
 
-from app.api.routes import users_router, auth_router, course_router, admin
+from app.api.routes import users_router, auth_router, course_router, admin_router
 from app.core.config import settings
 
 app = FastAPI(
     title=settings.app.APP_NAME,
 )
 
-app.include_router(users.router)
-app.include_router(auth.router)
-app.include_router(course.router)
-app.include_router(admin.router)
+app.include_router(users_router.router)
+app.include_router(auth_router.router)
+app.include_router(course_router.router)
+app.include_router(admin_router.router)
 
 
 @app.get("/")
