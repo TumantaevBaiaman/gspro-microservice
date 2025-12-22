cd0e76bd6db9a8585a40a912d270d9fedbe724be
TumantaevBaiaman <tumantaev.baiaman.space@gmail.com>
2025-12-22 23:58:27 +0600

    refactor: course service run

--- course_service/src/presentation/grpc/container.py
+++ course_service/src/presentation/grpc/container.py
@@ -1,14 +1,3 @@
-from typing import TypedDict
-
-from src.application.services import (
-    CourseService,
-    AdminCategoryService,
-    AdminCourseService,
-    AdminModuleService,
-    AdminLessonService,
-    CategoryService
-)
-
 from src.infrastructure.db.mongo.repositories import (
     CourseRepository,
     AdminCategoryRepository,
@@ -18,35 +7,22 @@ from src.infrastructure.db.mongo.repositories import (
     CategoryRepository,
 )
 
+from src.application.services import (
+    CourseService,
+    AdminCategoryService,
+    AdminCourseService,
+    AdminModuleService,
+    AdminLessonService,
+    CategoryService,
+)
 
-class Services(TypedDict):
-    course: CourseService
-    admin_category: AdminCategoryService
-    admin_course: AdminCourseService
-    admin_module: AdminModuleService
-    admin_lesson: AdminLessonService
-    category: CategoryService
 
-
-def build_repositories():
+def build_services() -> dict[type, object]:
     return {
-        "course": CourseRepository(),
-        "admin_category": AdminCategoryRepository(),
-        "admin_course": AdminCourseRepository(),
-        "admin_module": AdminModuleRepository(),
-        "admin_lesson": AdminLessonRepository(),
-        "category": CategoryRepository(),
-    }
-
-
-def build_services() -> Services:
-    repos = build_repositories()
-
-    return {
-        "course": CourseService(repos["course"]),
-        "admin_category": AdminCategoryService(repos["admin_category"]),
-        "admin_course": AdminCourseService(repos["admin_course"]),
-        "admin_module": AdminModuleService(repos["admin_module"]),
-        "admin_lesson": AdminLessonService(repos["admin_lesson"]),
-        "category": CategoryService(repos["category"]),
+        CourseService: CourseService(CourseRepository()),
+        AdminCategoryService: AdminCategoryService(AdminCategoryRepository()),
+        AdminCourseService: AdminCourseService(AdminCourseRepository()),
+        AdminModuleService: AdminModuleService(AdminModuleRepository()),
+        AdminLessonService: AdminLessonService(AdminLessonRepository()),
+        CategoryService: CategoryService(CategoryRepository()),
     }
